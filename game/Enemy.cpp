3957e3f7d4c2202ba52c7c02a2bf72fd4af250f2
diag4nd <144257276+diag4nd@users.noreply.github.com>
2024-05-10 15:26:39 +0300

    Delete game directory

--- game/Enemy.cpp
+++
@@ -1,5 +0,0 @@
-#include "Enemy.h"
-
-Enemy::Enemy(int _y, int _x, int _h, int _w): Monkey(_y, _x, _h, _w), reward{100}, delay{0}
-{}
-
--- game/globals.cpp
+++
@@ -1,6 +0,0 @@
-#include "globals.h"
-
-int choice = 0;
-int tuneX = 10;
-int tuneY = 0;
-int ovrlDel = 3;
